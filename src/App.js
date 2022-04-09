427e25f0abfaeb425a0c58034c2566feee9bbeaa
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-09 12:11:28 +0600

    form, change, blur, submit, preventDefault

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import app from './firebase.init';
 const auth = getAuth(app);
 
 function App() {
+
+  const handleEmailBlur = event =>{
+    console.log(event.target.value);
+  };
+
+  const handlePasswordBlur = event =>{
+    console.log(event.target.value);
+  };
+
+  const handleFormSubmit = event =>{
+    console.log('Submited');
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <form action="">
-        <input className='inputField' type="text" placeholder='Enter Your Email'/>
-        <input className='inputField' type="password" name="" id="" placeholder='Enter Your Password'/>
+      <form action="" onSubmit={handleFormSubmit}>
+        <input onBlur={handleEmailBlur} className='inputField' type="email" placeholder='Enter Your Email'/>
+        <input onBlur={handlePasswordBlur} className='inputField' type="password" name="" id="" placeholder='Enter Your Password'/>
+        <br />
+        <input className='logIn' type="submit" value="Login" />
       </form>
     </div>
   );
