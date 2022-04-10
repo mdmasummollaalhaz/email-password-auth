ca825c2a50a2e5bd629509944cafd91e3c0b4e6e
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-10 07:50:43 +0600

    Toggle sign in & sign up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import app from './firebase.init';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -10,16 +10,24 @@ const auth = getAuth(app);
 
 function App() {
   const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
   const [error, setError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleEmailBlur = event => {
+    // console.log(event.target.value);
     setEmail(event.target.value);
   };
 
   const handlePasswordBlur = event => {
+    // console.log(event.target.value);
     setPassword(event.target.value);
+  };
+
+  const handleRegisteredChange = event => {
+    // console.log(event.target.checked);
+    setRegistered(event.target.checked);
   };
 
   const handleFormSubmit = event => {
@@ -39,21 +47,41 @@ function App() {
     setError('');
 
     // console.log('Submited', email, password);
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(result =>{
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(error =>{
+          console.error(error);
+          setError(error.message);
+        })
+
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+          setEmail('');
+          setPassword('');
+        })
+        .catch(error => {
+          console.error(error);
+          setError(error.message);
+        });
+    }
+
     event.preventDefault();
   };
 
   return (
     <div className="App">
       <div className="registration w-50 mx-auto mt-5">
-        <h2 className="text-primany">Registation From</h2>
+        <h2 className="text-primany">
+          Please {registered ? 'Login' : 'Register'}
+        </h2>
         <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
@@ -83,10 +111,17 @@ function App() {
               Please provide a valid password.
             </Form.Control.Feedback>
           </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              onChange={handleRegisteredChange}
+              type="checkbox"
+              label="Already Registered?"
+            />
+          </Form.Group>
           <p className="text-danger">{error}</p>
 
           <Button variant="primary" type="submit">
-            Submit
+            {registered ? 'Login Now' : 'Register Now'}
           </Button>
         </Form>
       </div>
